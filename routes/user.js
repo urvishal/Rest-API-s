63f0a953404fdfdc2b2a842132ff9911e191202d
Vishal Kumar <vishal312006@gmail.com>
2025-08-21 15:07:19 +0530

    New filess added

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {handleGetAllUsers, handleGetUserById, handleUpdateUserById,handleDeleteUserById,handleCreateNewUser} = require("../controllers/user");
+const router = express.Router();
+
+//Routes
+router.get("/", handleGetAllUsers);
+
+router
+  .route("/:id")
+  .get(handleGetUserById)
+  .patch(handleUpdateUserById)
+  .delete(handleDeleteUserById);
+
+router.post("/", handleCreateNewUser);
+
+module.exports = router;

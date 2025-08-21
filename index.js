e4c90a0df3caa6910dd2bc648371064a0922ef9f
vishal kumar <vishal312006@gmail.com>
2025-08-21 21:19:28 +0530

    conflict changed

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
 
 //Routes
-<<<<<<< HEAD
-app.use("/api/users", userRouter);
-=======
 app.use("/user", userRouter);
->>>>>>> 63f0a953404fdfdc2b2a842132ff9911e191202d
 
 app.listen(PORT, console.log(`Server started at PORT ${PORT}`));
